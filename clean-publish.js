5aa3d36ca3424cef32eaf556c6e59734ec5496e9
shashkovdanil <shashkovdanil@gmail.com>
2018-07-11 20:59:34 +0500

    Move to async

--- clean-publish.js
+++ clean-publish.js
@@ -3,40 +3,41 @@
 const fs = require('fs');
 const fse = require('fs-extra');
 const omit = require('lodash.omit');
-const {
-  spawn,
-  execSync,
-} = require('child_process');
+const { spawn } = require('child_process');
 
 const IGNORE_FILES = require('./ignore-files');
 const IGNORE_FIELDS = require('./ignore-fields');
 
 (function () {
-  const tmp = fs.mkdtempSync('tmp');
   const src = './'
   const packageJSON = 'package.json'
-  fs.readdirSync(src).forEach(i => {
-    if (i !== tmp) {
-      if (IGNORE_FILES.indexOf(i) == -1 || fs.statSync(i).isDirectory()) {
-        fse.copy(i, `${tmp}/${i}`)
-          .then(() => {
-            if (i === packageJSON) {
+  fs.mkdtemp('tmp', (err, tmpDir) => {
+    fs.readdir(src, (err, files) => {
+      files
+        .filter((file) => {
+          for (let i = 0; i < IGNORE_FILES.length; i += 1) {
+            return file.search(IGNORE_FILES[0]) === -1
+          }
+        })
+        .forEach(file => {
+          fse.copy(file, `${tmpDir}/${file}`, (err) => {
+            if (file === packageJSON) {
               fse.readJson(packageJSON, (err, obj) => {
-                fse.writeJsonSync(`./${tmp}/${packageJSON}`, omit(obj, IGNORE_FIELDS), {
+                fse.writeJson(`./${tmpDir}/${packageJSON}`, omit(obj, IGNORE_FIELDS), {
                   spaces: 2
                 })
               })
             }
-          });
-      }
-    }
+          })
+        })
+    })
+    process.chdir(tmpDir);
+    const publish = spawn('npm', ['publish'], {
+      stdio: 'inherit'
+    });
+    publish.on('exit', () => {
+      fse.remove(tmpDir);
+    })
+    process.chdir('../');
   })
-  process.chdir(tmp);
-  const publish = spawn('npm', ['publish'], {
-    stdio: 'inherit'
-  });
-  publish.on('exit', () => {
-    fse.removeSync(tmp);
-  })
-  process.chdir('../');
 })();
