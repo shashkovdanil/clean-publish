6babb0577eb4e2121e58f2ab7e717001bf8aad86
shashkovdanil <shashkovdanil@gmail.com>
2018-07-11 21:12:33 +0500

    Error handling

--- clean-publish.js
+++ clean-publish.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
 const fs = require('fs');
 const fse = require('fs-extra');
 const omit = require('lodash.omit');
@@ -12,7 +13,15 @@ const IGNORE_FIELDS = require('./ignore-fields');
   const src = './'
   const packageJSON = 'package.json'
   fs.mkdtemp('tmp', (err, tmpDir) => {
+    if (err) {
+      console.log(chalk.red(err))
+      process.exit()
+    }
     fs.readdir(src, (err, files) => {
+      if (err) {
+        console.log(chalk.red(err))
+        process.exit()
+      }
       files
         .filter((file) => {
           for (let i = 0; i < IGNORE_FILES.length; i += 1) {
@@ -21,8 +30,16 @@ const IGNORE_FIELDS = require('./ignore-fields');
         })
         .forEach(file => {
           fse.copy(file, `${tmpDir}/${file}`, (err) => {
+            if (err) {
+              console.log(chalk.red(err))
+              process.exit()
+            }
             if (file === packageJSON) {
               fse.readJson(packageJSON, (err, obj) => {
+                if (err) {
+                  console.log(chalk.red(err))
+                  process.exit()
+                }
                 fse.writeJson(`./${tmpDir}/${packageJSON}`, omit(obj, IGNORE_FIELDS), {
                   spaces: 2
                 })
@@ -36,7 +53,13 @@ const IGNORE_FIELDS = require('./ignore-fields');
       stdio: 'inherit'
     });
     publish.on('exit', () => {
-      fse.remove(tmpDir);
+      fse.remove(tmpDir, (err) => {
+        if (err) {
+          console.log(chalk.red(err))
+          process.chdir('../');
+          process.exit()
+        }
+      });
     })
     process.chdir('../');
   })
