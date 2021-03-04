983f4cd9a7679596cb72bb6bcb1bbabcb7a6a7d3
Andrey Sitnik <andrey@sitnik.ru>
2021-03-04 14:02:51 -0500

    Simplify test runner

--- test/clear-package-json.test.js
+++ test/clear-package-json.test.js
@@ -6,23 +6,27 @@ const path = require('path')
 const cleanPackageJSON = require('./clean-package.json')
 const cleanPublishConfig = require('./clean-publish-config.json')
 
-const packageJSONPath = path.join(__dirname, 'package', 'package.min.json')
-const cleanPublishConfigPath = path.join(__dirname, 'package', '.clean-publish')
+const binPath = path.join(__dirname, '..', 'clear-package-json.js')
+const packagePath = path.join(__dirname, 'package')
+const packageJSONPath = path.join(packagePath, 'package.json')
+const minPackageJSONPath = path.join(packagePath, 'package.min.json')
+const cleanPublishConfigPath = path.join(packagePath, '.clean-publish')
 
 // Removing artifacts if tests are failed.
 afterAll(() => {
-  fse.remove(packageJSONPath)
+  fse.remove(minPackageJSONPath)
   fse.remove(cleanPublishConfigPath)
 })
 
 it('test clear-package-json function', done => {
-  spawn('npm', ['run', 'test-clear-package-json'], {
+  spawn(binPath, [packageJSONPath, '-o', minPackageJSONPath], {
+    cwd: packagePath,
     stdio: 'inherit'
   }).on('close', () => {
-    fse.readJson(packageJSONPath, (readJSONErr, obj) => {
+    fse.readJson(minPackageJSONPath, (readJSONErr, obj) => {
       if (readJSONErr) return done(readJSONErr)
       expect(obj).toEqual(cleanPackageJSON)
-      fse.removeSync(packageJSONPath)
+      fse.removeSync(minPackageJSONPath)
       done()
     })
   })
@@ -34,15 +38,16 @@ it('test clear-package-json to get fields from config file', done => {
     JSON.stringify(cleanPublishConfig),
     'utf8'
   )
-  spawn('npm', ['run', 'test-clear-package-json'], {
+  spawn(binPath, [packageJSONPath, '-o', minPackageJSONPath], {
+    cwd: packagePath,
     stdio: 'inherit'
   }).on('close', () => {
-    fse.readJson(packageJSONPath, (readJSONErr, obj) => {
+    fse.readJson(minPackageJSONPath, (readJSONErr, obj) => {
       if (readJSONErr) return done(readJSONErr)
       const cleanerPackageJSON = Object.assign({}, cleanPackageJSON)
       delete cleanerPackageJSON.collective
       expect(obj).toEqual(cleanerPackageJSON)
-      fse.removeSync(packageJSONPath)
+      fse.removeSync(minPackageJSONPath)
       fse.removeSync(cleanPublishConfigPath)
       done()
     })
