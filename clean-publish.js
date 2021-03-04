e733b960da083d1164ecdf8abc4eb310be54640b
Andrey Sitnik <andrey@sitnik.ru>
2021-03-04 11:56:25 -0500

    Remove yargs

--- clean-publish.js
+++ clean-publish.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs')
 const {
   createTempDirectory,
   readSrcDirectory,
@@ -15,44 +14,54 @@ const {
 } = require('./core')
 const getConfig = require('./get-config')
 
-const { argv } = yargs
-  .usage('$0')
-  .option('files', {
-    type: 'array',
-    desc: 'One or more exclude files'
-  })
-  .option('fields', {
-    type: 'array',
-    desc: 'One or more exclude package.json fields'
-  })
-  .option('without-publish', {
-    type: 'boolean',
-    desc: 'Clean package without npm publish'
-  })
-  .option('package-manager', {
-    types: 'string',
-    default: 'npm',
-    desc: 'Package manager to use'
-  })
-  .option('access', {
-    types: 'string',
-    desc: 'Whether the npm registry publishes ' +
-      'this package as a public package, or restricted'
-  })
-  .option('before-script', {
-    types: 'string',
-    desc: 'Run script on the to-release dir before npm publish'
-  })
+const HELP = 'npx clean-publish\n' +
+  '\n' +
+  'Options:\n' +
+  '  --help             Show help\n' +
+  '  --version          Show version number\n' +
+  '  --files            One or more exclude files\n' +
+  '  --fields           One or more exclude package.json fields\n' +
+  '  --without-publish  Clean package without npm publish\n' +
+  '  --package-manager  Package manager to use\n' +
+  '  --access           Whether the npm registry publishes this package\n' +
+  '                     as a public package, or restricted\n' +
+  '  --before-script    Run script on the to-release dir before npm\n' +
+  '                     publish'
 
 const options = {}
 let tempDirectoryName
 
 function handleOptions () {
-  Object.assign(options, argv, {
-    withoutPublish: argv['without-publish'],
-    packageManager: argv['package-manager']
-  })
-  if (options['_'].length === 0) {
+  options.packageManager = 'npm'
+  for (let i = 2; i < process.argv.length; i++) {
+    if (process.argv[i] === '--help') {
+      console.log(HELP)
+      process.exit(0)
+    } else if (process.argv[i] === '--version') {
+      console.log(require('./package.json').version)
+      process.exit(0)
+    } else if (process.argv[i] === '--without-publish') {
+      options.withoutPublish = true
+    } else if (process.argv[i] === '--package-manager') {
+      options.packageManager = process.argv[i + 1]
+      i += 1
+    } else if (process.argv[i] === '--before-script') {
+      options.beforeScript = process.argv[i + 1]
+      i += 1
+    } else if (process.argv[i] === '--access') {
+      options.access = process.argv[i + 1]
+      i += 1
+    } else if (process.argv[i] === '--files') {
+      options.files = process.argv[i + 1].split(/,\s*/)
+      i += 1
+    } else if (process.argv[i] === '--fields') {
+      options.fields = process.argv[i + 1].split(/,\s*/)
+      i += 1
+    } else {
+      options['_'] = process.argv[i]
+    }
+  }
+  if (!options['_']) {
     return getConfig().then(config => {
       Object.assign(options, config)
     })
