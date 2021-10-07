00641899b99e235842da5e090d4d45dcb3320c38
dangreen <danon0404@gmail.com>
2021-10-08 01:03:12 +0700

    Some tests cleanup

--- test/clear-package-json.test.js
+++ test/clear-package-json.test.js
@@ -1,15 +1,13 @@
-import spawn from 'cross-spawn'
-import fs from 'fs'
 import fse from 'fs-extra'
 import { join } from 'path'
 import { fileURLToPath } from 'url'
 
+import { spawn } from './utils.js'
+
 const dirname = join(fileURLToPath(import.meta.url), '..')
 
-const cleanPackageJSON = fse.readJSONSync(join(dirname, './clean-package.json'))
-const cleanPublishConfig = fse.readJSONSync(
-  join(dirname, './clean-publish-config.json')
-)
+const cleanPackageJSONPath = join(dirname, './clean-package.json')
+const cleanPublishConfigSrcPath = join(dirname, './clean-publish-config.json')
 
 const binPath = join(dirname, '..', 'clear-package-json.js')
 const packagePath = join(dirname, 'package')
@@ -17,44 +15,54 @@ const packageJSONPath = join(packagePath, 'package.json')
 const minPackageJSONPath = join(packagePath, 'package.min.json')
 const cleanPublishConfigPath = join(packagePath, '.clean-publish')
 
+let cleanPackageJSON
+let cleanPublishConfig
+
+beforeAll(async () => {
+  [cleanPackageJSON, cleanPublishConfig] = await Promise.all([
+    fse.readJSON(cleanPackageJSONPath),
+    fse.readJSON(cleanPublishConfigSrcPath)
+  ])
+})
+
 // Removing artifacts if tests are failed.
-afterAll(() => {
-  fse.remove(minPackageJSONPath)
-  fse.remove(cleanPublishConfigPath)
+afterAll(async () => {
+  await Promise.all([
+    fse.remove(minPackageJSONPath),
+    fse.remove(cleanPublishConfigPath)
+  ])
 })
 
-it('test clear-package-json function', done => {
-  spawn(binPath, [packageJSONPath, '-o', minPackageJSONPath], {
-    cwd: packagePath,
-    stdio: 'inherit'
-  }).on('close', () => {
-    fse.readJson(minPackageJSONPath, (readJSONErr, obj) => {
-      if (readJSONErr) return done(readJSONErr)
-      expect(obj).toEqual(cleanPackageJSON)
-      fse.removeSync(minPackageJSONPath)
-      done()
-    })
+it('test clear-package-json function', async () => {
+  await spawn(binPath, [packageJSONPath, '-o', minPackageJSONPath], {
+    cwd: packagePath
   })
+
+  const obj = await fse.readJSON(minPackageJSONPath)
+
+  expect(obj).toEqual(cleanPackageJSON)
+
+  await fse.remove(minPackageJSONPath)
 })
 
-it('test clear-package-json to get fields from config file', done => {
-  fs.writeFileSync(
+it('test clear-package-json to get fields from config file', async () => {
+  await fse.writeFile(
     cleanPublishConfigPath,
     JSON.stringify(cleanPublishConfig),
     'utf8'
   )
-  spawn(binPath, [packageJSONPath, '-o', minPackageJSONPath], {
+  await spawn(binPath, [packageJSONPath, '-o', minPackageJSONPath], {
     cwd: packagePath,
-    stdio: 'inherit'
-  }).on('close', () => {
-    fse.readJson(minPackageJSONPath, (readJSONErr, obj) => {
-      if (readJSONErr) return done(readJSONErr)
-      const cleanerPackageJSON = Object.assign({}, cleanPackageJSON)
-      delete cleanerPackageJSON.collective
-      expect(obj).toEqual(cleanerPackageJSON)
-      fse.removeSync(minPackageJSONPath)
-      fse.removeSync(cleanPublishConfigPath)
-      done()
-    })
   })
+
+  const obj = await fse.readJSON(minPackageJSONPath)
+  const cleanerPackageJSON = Object.assign({}, cleanPackageJSON)
+  delete cleanerPackageJSON.collective
+
+  expect(obj).toEqual(cleanerPackageJSON)
+
+  await Promise.all([
+    fse.remove(minPackageJSONPath),
+    fse.remove(cleanPublishConfigPath)
+  ])
 })
