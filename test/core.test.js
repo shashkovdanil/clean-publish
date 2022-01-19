d6192107675ae964c6c83006fba99bea863dfd99
dangreen <danon0404@gmail.com>
2022-01-20 00:17:25 +0700

    feat: deep files filter

--- test/core.test.js
+++ test/core.test.js
@@ -4,7 +4,7 @@ import { test } from 'uvu'
 import { join } from 'path'
 import fse from 'fs-extra'
 
-import { clearPackageJSON, getReadmeUrlFromRepository } from '../core.js'
+import { clearPackageJSON, getReadmeUrlFromRepository, createFilesFilter } from '../core.js'
 
 const dirname = join(fileURLToPath(import.meta.url), '..')
 const srcPackageJSONPath = join(dirname, 'package', 'package.json')
@@ -75,6 +75,23 @@ test('getReadmeUrlFromRepository handles repo object', () => {
     }),
     readme
   )
+})
+
+test('createFilesFilter creates correct filter', () => {
+  const filter = createFilesFilter([
+    /^\.config.js(on)?/,
+    '.DS_Store',
+    '**/*.spec.js'
+  ])
+
+  is(filter('.config.json'), false)
+  is(filter('test/.config.json'), false)
+  is(filter('.DS_Store'), false)
+  is(filter('dist/.DS_Store'), false)
+  is(filter('src/core.spec.js'), false)
+  is(filter('src/cli/options.spec.js'), false)
+  is(filter('src/cli/options.js'), true)
+  is(filter('dist/index.js'), true)
 })
 
 test.run()
